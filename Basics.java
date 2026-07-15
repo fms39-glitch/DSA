74fbeb88fc329600e541261cbdaa2cc90d833ebe
fms39-glitch <fms39@njit.edu>
2026-07-14 22:00:04 -0400

    Basics of java

--- Basics.java
+++ Basics.java
@@ -3,3 +3,12 @@ public class Basics {
         System.out.println("Hello World");
     }
 }
+
+// System -> class
+// out -> static variable of type PrintStream
+// println -> method of type PrintStream
+// system.files.out.println(); ---- will print in the particular file.
+// if we dont provide extension to the file -> default is .java
+// Scanner scan = new Scanner(System.in); ---similarly if you replace system.in
+// -> file_name it will read that particular file for input.
+// System.out.println(scan.nextInt());
