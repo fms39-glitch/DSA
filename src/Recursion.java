0b09e4555a088a4192657605d3f57f0c56f7d2d1
fms39-glitch <fms39@njit.edu>
2026-09-10 22:54:57 -0400

    Solved problem 2094 on leet code

--- src/Recursion.java
+++ src/Recursion.java
@@ -114,12 +114,13 @@ public class Recursion{
     }
 
 
-    // --- Solved 5 leet code problems ---
+    // --- Solved 6 leet code problems ---
         // 509. Fibonnacci (easy recursive)
         // 231. Power of Two (easy recursive)
         // 326. Power of Three (easy recursive)
         // 342. power of Four (easy  recursive)
         //  50. Pow(x, n) (medium)
+        //2094. Finding 3-Digit Even Numbers (Easy)
 
 
 
