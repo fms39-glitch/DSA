094f635a01175565bc99b24f63312b129fdeb92b
fms39-glitch <fms39@njit.edu>
2026-09-15 09:33:55 -0400

    Time Complexity and Big O Notation

---
+++ src/TimeComplexity.java
@@ -0,0 +1,33 @@
+//Time complexity
+//--> Measure of how the running time of an algorithm increases
+//    with the size of the input data.
+
+//searching an element in a sorted Arrat
+// linear search & Binary Search
+// 1) Linear Search. Array: {5,7, 9, 12, 17}
+//    - In this we set the target-> 5 and comare 
+//      it with the each element in the array.
+//    - If the lenght of an Array increases & the 
+//      tarfet will be at the end, it will take more time to 
+
+
+// 2) Binary search. {5, 7, 9, 12, 17, 20, 21}
+//    - Target is 8.
+//    - Finding the mid value. (Starting index + End Index) / 2
+//    - After finding the mid if the target is less than midvalue 
+//      the right side values are not important
+//    - Then you make the mid your end and again find the mid value 
+//      and see where the target lies?  
+
+
+// Big O notation.
+//    - It is represented by big O.
+//    1) O(1)   eg. In the linear search the n(number of elements)
+//      will increases, 
+//    2) O(log n)  eg. Binary Search --> The O notation will be between O(1) - O(n), 
+//      what is between tem it's O(log n)
+//    3) O() 
+//    4) O()  
+//    5) O()  
+//    6) O()   
+
