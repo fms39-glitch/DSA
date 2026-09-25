345d052dc1065a7559e0b1fd098a3fc38e3d5d8c
fms39-glitch <fms39@njit.edu>
2026-09-25 17:43:39 -0400

    Bubble sort

---
+++ src/sorting/Bubble.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Bubble {
+    public static int[] bubble(int[] a) {
+        // Fixed: replaced commas with semicolons and corrected "length"
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 1; j < a.length - i; j++) {
+                // Fixed: Corrected the element comparison and swap sequence
+                if (a[j-1] > a[j]) {
+                    int temp = a[j-1];
+                    a[j-1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a; // Fixed: Added the missing return statement
+    }
+
+    public static void main(String args[]){
+        int[] a = {3,2,1};
+        System.out.println(Arrays.toString(bubble(a)));
+    }
+}
